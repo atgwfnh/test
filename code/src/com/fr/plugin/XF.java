9e04822a3110df30bf057456451385a82feb63d4
atgwfnh <atgwfnh@163.com>
2021-04-10 12:26:19 +0800

    xf
    Signed-off-by: atgwfnh <atgwfnh@163.com>

---
+++ code/src/com/fr/plugin/XF.java
@@ -0,0 +1,45 @@
+package com.fr.plugin;
+
+import com.fr.plugin.transform.ExecuteFunctionRecord;
+import com.fr.plugin.transform.FunctionRecorder;
+import com.fr.script.AbstractFunction;
+
+@FunctionRecorder
+public class XF extends AbstractFunction {
+    public static int getGongYueShu(int a, int b) {
+        int t = 0;
+        if(a < b){
+            t = a;
+            a = b;
+            b = t;
+        }
+        int c = a % b;
+        if(c == 0){
+            return b;
+        }else{
+            return getGongYueShu(b, c);
+        }
+    }
+
+    @Override
+    @ExecuteFunctionRecord
+    public Object run(Object[] args) {
+        String result = "";
+        Object para;
+        for (int i = 0; i < args.length; i++) {
+            para = args[i].toString();
+            String[] array = new String[2];
+            array = ((String) para).split("\\.");
+            int a = Integer.parseInt(array[0]);
+            int b = Integer.parseInt(array[1]);
+            int length = array[1].length();
+            int FenZi = (int) (a * Math.pow(10, length) + b);
+            int FenMu = (int) Math.pow(10, length);
+            int MaxYueShu = getGongYueShu(FenZi, FenMu);
+            result += FenZi / MaxYueShu + "/" + FenMu / MaxYueShu;
+        }
+        return result;
+    }
+
+}
+
